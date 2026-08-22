5858fe5d3fcb4cc8b487456b50ca4bb1f41e174f
popam482 <popamaria482@gmail.com>
2026-08-22 14:50:33 +0300

    multiple linear regression

--- 08-Linear_regression.py
+++ 08-Linear_regression.py
@@ -17,3 +17,21 @@ model.fit(X, y)
 estimated_price = model.predict([[70]])
 
 print(estimated_price)
+
+X1 = np.array([
+    [50, 2, 10],
+    [30, 1, 40],
+    [80, 3, 30],
+    [65, 2, 35]
+])
+
+y1 = np.array([70000, 450000, 110000, 65000])
+
+model1 = LinearRegression()
+model1.fit(X1, y1)
+
+new_apartment = np.array([[70, 4, 45]])
+
+y_new_apartment = model1.predict(new_apartment)
+
+print(y_new_apartment)
