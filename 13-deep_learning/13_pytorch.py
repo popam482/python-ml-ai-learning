cc101e8c29623badb371e43b27a3a7d46417aac4
popam482 <popamaria482@gmail.com>
2026-09-07 19:23:38 +0300

    pythorch update

--- 13-deep_learning/13_pytorch.py
+++ 13-deep_learning/13_pytorch.py
@@ -1,6 +1,6 @@
 import torch
 
-#define tensor and activate gradients
+# define tensor and activate gradients
 x = torch.tensor(2.0, requires_grad=True)
 
 # forward pass
@@ -10,4 +10,14 @@ y = x ** 2 + 3 * x + 5
 y.backward()
 
 print(f"Function value y(2): {y.item()}")
-print(f"dy/dx derivative for x=2: {y.grad.item()}")
+print(f"dy/dx derivative for x=2: {x.grad.item()}")
+
+x1 = torch.tensor(3.0, requires_grad=True)
+y1 = torch.tensor(2.0, requires_grad=True)
+
+z = 2 * x1 ** 3 - 4 * y1 ** 2 + 7
+
+z.backward()
+
+print(f"dy/dx derivative for x=3: {x1.grad.item()}")
+print(f"dx/dy derivative for y=2: {y1.grad.item()}")
